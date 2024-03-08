b6d148dd6536ea50afee7b1979f2f9d6f0cd3a20
RainyHush <yang1999325@gmail.com>
2024-03-08 10:27:20 +0800

    update SingBox_Template_script.js

--- SingBox_Template_script.js
+++ SingBox_Template_script.js
@@ -11,13 +11,13 @@ config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
   if (['其它节点'].includes(i.tag)) {
-  i.outbounds.push(...proxies.filter(p => !/港|香港|hk|hongkong|kong kong|🇨🇳|济南|杭州|徐州|浙江|🇯🇵|日本|東京|jp|Japan|🇭🇰|台|台灣|tw|taiwan|🇹🇼|新|sg|singapore|🇸🇬|美|us|Oregon|Oregon|unitedstates|united states|united states|🇺🇸|韩|kr|서울|Korea|🇰🇷/i.test(p.tag)).map(p => p.tag))
+  i.outbounds.push(...proxies.filter(p => !/港|香港|hk|hongkong|kong kong|🇨🇳|济南|杭州|广州|徐州|浙江|🇯🇵|日本|東京|jp|Japan|🇭🇰|台|台灣|tw|taiwan|🇹🇼|新|sg|singapore|🇸🇬|美|us|Oregon|Oregon|unitedstates|united states|united states|🇺🇸|韩|kr|서울|Korea|🇰🇷/i.test(p.tag)).map(p => p.tag))
   }
   if (['香港节点'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /港|香港|hk|hongkong|kong kong|🇭🇰/i.test(p.tag)).map(p => p.tag))
   }
   if (['中国节点'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /浙江|徐州|杭州|济南|🇨🇳/i.test(p.tag)).map(p => p.tag))
+    i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|杭州|济南|🇨🇳/i.test(p.tag)).map(p => p.tag))
   }
   if (['台湾节点'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /台|台灣|tw|taiwan|🇹🇼/i.test(p.tag)).map(p => p.tag))
