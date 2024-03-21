7209e3e756b22f7b987d2a71c2695779c6450fa4
RainyHush <yang1999325@gmail.com>
2024-03-21 15:33:39 +0800

    update

--- sing box_Template_script.js
+++ sing box_Template_script.js
@@ -1,42 +1,58 @@
-
 let config = JSON.parse($files[0])
 let proxies = await produceArtifact({
-  name: '组合订阅',
-  type: 'collection',//collection or subscription
-  platform: 'sing-box',
-  produceType: 'internal',
+	name: '组合订阅',
+	type: 'collection', //collection or subscription
+	platform: 'sing-box',
+	produceType: 'internal',
 })
 
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['Others'].includes(i.tag)) {
-  i.outbounds.push(...proxies.filter(p => !/港|香港|hk|Hong Kong|HK|🇨🇳|济南|杭州|鞍山|广州|徐州|浙江|🇩🇪|Germany|德国|🇯🇵|日本|東京|jp|Japan|🇭🇰|台|台灣|tw|taiwan|🇹🇼|新|sg|singapore|🇸🇬|美|us|America|Oregon|unitedstates|united states|united states|🇺🇸|韩|kr|서울|Korea|🇰🇷/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['Hong Kong'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /港|香港|hk|Hong Kong|HK|🇭🇰/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['China'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|杭州|济南|🇨🇳/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['TaiWan'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /台|台灣|tw|taiwan|🇹🇼/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['Japan'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /日本|東京都|jp|japan|🇯🇵/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['Singapore'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['America'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /美国|🇺🇸|us|America|Oregon|united states/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['Korea'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /韩|kr|서울|Korea|🇰🇷/i.test(p.tag)).map(p => p.tag))
-  }
-  if (['Germany'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /德|de|DE|Germany|🇩🇪/i.test(p.tag)).map(p => p.tag))
-  }
+	if (['Others'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|🇭🇰|浙江|徐州|广州|鞍山|杭州|济南|🇨🇳|台湾|tw|TW|TaiWan|🇹🇼|日本|jp|JP|Japan|🇯🇵|新加坡|狮城|sg|SG|Singapore|🇸🇬|美国|us|US|America|united states|🇺🇸|阿根廷|ar|AR|Argentina|🇦🇷|德国|de|DE|Germany|🇩🇪|韩国|kr|KR|Korea|🇰🇷|英国|gb|GB|Great Britain|United Kiongdom|🇬🇧/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Hong Kong'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /香港|hk|HK|Hong Kong|🇭🇰/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['China'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|杭州|济南|🇨🇳/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['TaiWan'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan|🇹🇼/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Japan'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan|🇯🇵/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Singapore'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore|🇸🇬/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['America'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /美国|us|US|America|united states|🇺🇸/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Korea'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea|🇰🇷/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Germany'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /德国|de|DE|Germany|🇩🇪/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Great Britain'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /英国|gb|GB|Great Britain|United Kiongdom|🇬🇧/i.test(p.tag))
+			.map(p => p.tag))
+	}
+	if (['Argentina'].includes(i.tag)) {
+		i.outbounds.push(...proxies.filter(p => /阿根廷|ar|AR|Argentina|🇦🇷/i.test(p.tag))
+			.map(p => p.tag))
+	}
 })
 
 
