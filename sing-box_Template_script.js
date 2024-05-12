c2dc35bd0792865f92bfc8d3d00e6a2e36429ce6
RainyHush <yang1999325@gmail.com>
2024-05-12 20:30:07 +0800

    modified:   sing-box_Template_script.js

--- sing-box_Template_script.js
+++ sing-box_Template_script.js
@@ -18,7 +18,7 @@ config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
 	if (['Others'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|🇭🇰|浙江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|🇨🇳|台湾|tw|TW|TaiWan|🇹🇼|日本|jp|JP|Japan|🇯🇵|新加坡|狮城|sg|SG|Singapore|🇸🇬|美国|us|US|America|united states|🇺🇸|德国|de|DE|Germany|🇩🇪|韩国|kr|KR|Korea|🇰🇷/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|美国|us|US|America|united states|德国|de|DE|Germany|韩国|kr|KR|Korea/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['Hong Kong'].includes(i.tag)) {
@@ -26,31 +26,31 @@ config.outbounds.map(i => {
 			.map(p => p.tag))
 	}
 	if (['China'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|襄阳|武汉|杭州|济南|🇨🇳/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['TaiWan'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan|🇹🇼/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['Japan'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan|🇯🇵/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['Singapore'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore|🇸🇬/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['America'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /美国|us|US|America|united states|🇺🇸/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /美国|us|US|America|united states/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['Korea'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea|🇰🇷/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea/i.test(p.tag))
 			.map(p => p.tag))
 	}
 	if (['Germany'].includes(i.tag)) {
-		i.outbounds.push(...proxies.filter(p => /德国|de|DE|Germany|🇩🇪/i.test(p.tag))
+		i.outbounds.push(...proxies.filter(p => /德国|de|DE|Germany/i.test(p.tag))
 			.map(p => p.tag))
 	}
 })
