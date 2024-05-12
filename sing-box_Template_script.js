6ad317ae5fc14b10b8b82f2c8b29b28358f81661
RainyHush <yang1999325@gmail.com>
2024-05-12 20:39:00 +0800

    modified:   sing-box_Template.json 	modified:   sing-box_Template_script.js

--- sing-box_Template_script.js
+++ sing-box_Template_script.js
@@ -17,39 +17,39 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-	if (['Others'].includes(i.tag)) {
+	if (['其他'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|美国|us|US|America|united states|德国|de|DE|Germany|韩国|kr|KR|Korea/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['Hong Kong'].includes(i.tag)) {
+	if (['香港'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /香港|hk|HK|Hong Kong/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['China'].includes(i.tag)) {
+	if (['中国'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['TaiWan'].includes(i.tag)) {
+	if (['台湾'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['Japan'].includes(i.tag)) {
+	if (['日本'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['Singapore'].includes(i.tag)) {
+	if (['新加坡'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['America'].includes(i.tag)) {
+	if (['美国'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /美国|us|US|America|united states/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['Korea'].includes(i.tag)) {
+	if (['韩国'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea/i.test(p.tag))
 			.map(p => p.tag))
 	}
-	if (['Germany'].includes(i.tag)) {
+	if (['德国'].includes(i.tag)) {
 		i.outbounds.push(...proxies.filter(p => /德国|de|DE|Germany/i.test(p.tag))
 			.map(p => p.tag))
 	}
