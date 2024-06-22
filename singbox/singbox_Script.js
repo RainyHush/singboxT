30aaf11e7d7e0807ad1b03a3a43a7315f90c0ab9
RainyHush <yang1999325@gmail.com>
2024-06-22 18:45:43 +0800

    update

--- singbox/singbox_Script.js
+++ singbox/singbox_Script.js
@@ -16,39 +16,39 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['其他'].includes(i.tag)) {
+  if (['Other'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|德国|de|DE|Germany|韩国|kr|KR|Korea|美国|us|US|America/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['香港'].includes(i.tag)) {
+  if (['Hong Kong'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /香港|hk|HK|Hong Kong/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['中国'].includes(i.tag)) {
+  if (['China'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['台湾'].includes(i.tag)) {
+  if (['TaiWan'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['日本'].includes(i.tag)) {
+  if (['Japan'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['狮城'].includes(i.tag)) {
+  if (['Singapore'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['韩国'].includes(i.tag)) {
+  if (['Korea'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['美国'].includes(i.tag)) {
+  if (['America'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /美国|us|US|America/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['德国'].includes(i.tag)) {
+  if (['Germany'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /德国|de|DE|Germany/i.test(p.tag))
       .map(p => p.tag))
   }
